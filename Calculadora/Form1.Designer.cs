ca38a80fcd33c5a0816ac1274efb2e343ab22435
Daniel <dandan.alves2009@gmail.com>
2026-03-06 09:56:46 -0300

    Alteração da cor da fonte das labels e dos botões

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -47,18 +47,21 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(419, 9);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(408, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 38);
+            this.label1.Size = new System.Drawing.Size(207, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Calculadora";
-            
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label2.Location = new System.Drawing.Point(276, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(321, 31);
@@ -68,7 +71,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.Location = new System.Drawing.Point(276, 165);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(313, 31);
@@ -78,25 +83,27 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(606, 83);
+            this.textBox1.Location = new System.Drawing.Point(606, 93);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(96, 41);
+            this.textBox1.Size = new System.Drawing.Size(116, 31);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(606, 155);
+            this.textBox2.Location = new System.Drawing.Point(606, 165);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(96, 41);
+            this.textBox2.Size = new System.Drawing.Size(116, 31);
             this.textBox2.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label4.Location = new System.Drawing.Point(381, 366);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(136, 31);
@@ -105,9 +112,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(285, 257);
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(258, 242);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 35);
+            this.button1.Size = new System.Drawing.Size(107, 50);
             this.button1.TabIndex = 7;
             this.button1.Text = "Somar";
             this.button1.UseVisualStyleBackColor = true;
@@ -115,9 +126,13 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(389, 257);
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(387, 242);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(98, 35);
+            this.button2.Size = new System.Drawing.Size(107, 50);
             this.button2.TabIndex = 9;
             this.button2.Text = "Subtrair";
             this.button2.UseVisualStyleBackColor = true;
@@ -125,9 +140,13 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(493, 257);
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.MediumPurple;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.BlueViolet;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumOrchid;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(517, 242);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(98, 35);
+            this.button3.Size = new System.Drawing.Size(107, 50);
             this.button3.TabIndex = 10;
             this.button3.Text = "Multiplicar";
             this.button3.UseVisualStyleBackColor = true;
@@ -135,9 +154,13 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(597, 257);
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(647, 242);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(98, 35);
+            this.button4.Size = new System.Drawing.Size(107, 50);
             this.button4.TabIndex = 11;
             this.button4.Text = "Dividir";
             this.button4.UseVisualStyleBackColor = true;
@@ -145,11 +168,12 @@
             // 
             // label5
             // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label5.Font = new System.Drawing.Font("Modern No. 20", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(534, 355);
+            this.label5.Location = new System.Drawing.Point(534, 366);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 42);
+            this.label5.Size = new System.Drawing.Size(112, 31);
             this.label5.TabIndex = 12;
             // 
             // button5
@@ -177,6 +201,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Calculadora.Properties.Resources.Sem_nome__1024_x_768_px_;
             this.CancelButton = this.button6;
             this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.button6);
